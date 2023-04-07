31d14f77c2a7b8b8c9a9b98fbb26ebfaaa35e492
Selasie5 <selasisepenu5@gmail.com>
2023-04-07 10:39:34 +0000

    Complete cars collections section

--- Stylesheets/main.js
+++ Stylesheets/main.js
@@ -1,7 +1,7 @@
 let menuOpen = document.getElementById('menuOpen');
 let menuClose = document.getElementById('menuClose');
 let transparentBackground = document.getElementById('transparentBackground');
-
+let filterSelect = document.getElementById('filterSelect');
 console.log(menuOpen);
 
 menuOpen.addEventListener('click', function()
@@ -13,3 +13,5 @@ menuClose.addEventListener('click', function()
 {
     transparentBackground.style.display = "none";
 })
+
+  
