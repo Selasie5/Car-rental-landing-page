56897488684031a01d5962256ed032bc7be7a4e5
Selasie5 <selasisepenu5@gmail.com>
2023-04-11 21:23:14 +0000

    Delete bootstrap files and complete page styling

--- Stylesheets/main.js
+++ Stylesheets/main.js
@@ -1,17 +1,3 @@
 let menuOpen = document.getElementById('menuOpen');
 let menuClose = document.getElementById('menuClose');
 let transparentBackground = document.getElementById('transparentBackground');
-let filterSelect = document.getElementById('filterSelect');
-console.log(menuOpen);
-
-menuOpen.addEventListener('click', function()
-{
-    transparentBackground.style.display = "block";
-})
-
-menuClose.addEventListener('click', function()
-{
-    transparentBackground.style.display = "none";
-})
-
-  
