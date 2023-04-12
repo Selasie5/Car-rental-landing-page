30cd60ffdabbc01bcd297cd34ed8dae1a0aed198
Selasie5 <selasisepenu5@gmail.com>
2023-04-12 20:53:08 +0000

    Develop mobile view for footer

--- Stylesheets/main.js
+++ Stylesheets/main.js
@@ -1,3 +1,14 @@
 let menuOpen = document.getElementById('menuOpen');
 let menuClose = document.getElementById('menuClose');
 let transparentBackground = document.getElementById('transparentBackground');
+
+menuOpen.addEventListener('click',()=>
+{
+    transparentBackground.style.display = 'block';
+    transparentBackground.style.opacity ='1';
+})
+menuClose.addEventListener('click', () =>
+{
+    transparentBackground.style.display = 'none';
+    transparentBackground.style.opacity ='0';
+})
